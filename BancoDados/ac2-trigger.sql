fc5612e1e843d253c7c484742e20e7550c86c3c4
Giovana Merguizo <104041362+GiMerguizo@users.noreply.github.com>
2023-11-17 11:24:10 -0300

    Update ac2-trigger.sql

--- BancoDados/ac2-trigger.sql
+++ BancoDados/ac2-trigger.sql
@@ -1,3 +1,4 @@
+use BANCO_AC2
 -- AC2 - Triggers
 
 -- Exercicio 1
@@ -19,9 +20,9 @@ AS
 BEGIN
 	declare @hoje int, @anoNascimento datetime
 	set @hoje = YEAR(GETDATE())
-	set @anoNascimento = YEAR(Personagem.DataNascimento)
+	SELECT @anoNascimento = YEAR(DataNascimento) FROM inserted
 
-	if @hoje - @anoNascimento > 18
+	if @hoje - @anoNascimento < 18
 	begin
 		raiserror('Cadastro somente após ter a idade maior que 18 anos', 10, 1)
 		rollback
@@ -29,7 +30,8 @@ BEGIN
 END
 
 -- Validação
-INSERT INTO Personagem values ('Amendoa', 'Um novo personagem ai', '05-06-2003', 2, 1, 0)
+INSERT INTO Personagem values ('Goku3.0', 'Goku boladaço sqn', '05-06-2003', 2, 1, 0)
+INSERT INTO Personagem values ('Amendoa', 'Um novo personagem ai', '22-03-2010', 3, 2, 45)
 
 select * from Personagem
 
@@ -39,17 +41,24 @@ ON Personagem
 AFTER INSERT
 AS
 BEGIN
-    declare @NomePersonagem varchar(), @NomeRaca varchar(), @NomeClasse varchar(), @NomeHabilidade varchar()
-    SET @detalhes = CONCAT()
+    declare @NomePersonagem varchar(100), @nomeRaca varchar(100), @nomeClasse varchar(100), @nomeHabilidade varchar(100), @detalhes varchar(1000)
+	declare @raca int, @classe int, @habilidade int
+	SELECT @NomePersonagem = Nome, @raca = IDRaca, @classe = IDClasse FROM inserted
+	SELECT @nomeRaca = Nome FROM Raca WHERE IDRaca = @raca
+	SELECT @nomeClasse = Nome FROM Classe WHERE IDClasse = @classe
+	SELECT @nomeHabilidade = Nome FROM Habilidade WHERE IDHabilidade = @habilidade
+    SET @detalhes = CONCAT('Personagem: ', @NomePersonagem, ' - Raca: ', @nomeRaca, ' - Classe: ', @nomeClasse, ' - Habilidade: ', @nomeHabilidade)
 
-INSERT INTO LogFull (Tabela, Operacao, Detalhes, DataEvento) VALUES
-('Personagem', 'Inserindo Personagem', 'Teste', getdate())
+	INSERT INTO LogFull (Tabela, Operacao, Detalhes, DataEvento) VALUES
+('Personagem', 'Inserindo Personagem', @detalhes, getdate())
 END
 
 
 --Validação 
 SELECT * FROM LogFull
 SELECT * FROM Personagem
+SELECT * FROM Raca
+SELECT * FROM Habilidade
 INSERT INTO Personagem values ('Goku2.0', 'O novo Goku', '01-01-2000', 1, 3, 75)
 
 -- Exercicio 4
@@ -59,17 +68,25 @@ AFTER INSERT
 AS
 BEGIN
     declare @hoje int, @anoNascimento datetime
+	declare @NomePersonagem varchar(100), @nomeRaca varchar(100), @nomeClasse varchar(100), @nomeHabilidade varchar(100), @detalhes varchar(1000)
+	declare @raca int, @classe int, @habilidade int
 	set @hoje = YEAR(GETDATE())
-	set @anoNascimento = YEAR(Personagem.DataNascimento)
+	SELECT @anoNascimento = YEAR(DataNascimento) FROM inserted
+	SELECT @NomePersonagem = Nome, @raca = IDRaca, @classe = IDClasse FROM inserted
+	SELECT @nomeRaca = Nome FROM Raca WHERE IDRaca = @raca
+	SELECT @nomeClasse = Nome FROM Classe WHERE IDClasse = @classe
+	SELECT @nomeHabilidade = Nome FROM Habilidade WHERE IDHabilidade = @habilidade
+	SET @detalhes = CONCAT('Personagem: ', @NomePersonagem, ' - Raca: ', @nomeRaca, ' - Classe: ', @nomeClasse, ' - Habilidade: ', @nomeHabilidade)
 
-	if @hoje - @anoNascimento > 18
+	if @hoje - @anoNascimento < 18
 	begin
 		raiserror('Cadastro somente após ter a idade maior que 18 anos', 10, 1)
 		rollback
-	end
+	end    
 
-    INSERT INTO LogFull (Tabela, Operacao, Detalhes, DataEvento) VALUES
-('Personagem', 'Inserindo Personagem', 'Teste', getdate())
+	INSERT INTO LogFull (Tabela, Operacao, Detalhes, DataEvento) VALUES
+('Personagem', 'Inserindo Personagem', @detalhes, getdate())
+
 END
 
 -- Exercicio 5
@@ -101,6 +118,7 @@ END
 -- Validação
 SELECT * FROM Raca;
 
+UPDATE Raca SET Nome = 'Orc2.0' WHERE Nome = 'Orc'
 SELECT * FROM LogFull WHERE Tabela = 'Raca';
 
 -- Exercicio 6
@@ -109,6 +127,17 @@ ON Habilidade
 AFTER DELETE
 AS
 BEGIN  
+	declare @nomeHabilidade varchar(100), @detalhes varchar(1000)
+	declare @IDhabilidade int, @poder int
+	SELECT @nomeHabilidade = Nome, @IDhabilidade = IDHabilidade, @poder = MultiplicadorPoder FROM deleted
+    SET @detalhes = CONCAT('ID: ', @IDhabilidade, ' - Nome: ', @nomeHabilidade, ' - Poder: ', @poder)
+
     INSERT INTO LogFull (Tabela, Operacao, Detalhes, DataEvento) VALUES
-('Habilidade', 'Excluindo Habilidade', 'Teste', getdate())
+('Habilidade', 'Excluindo Habilidade', @detalhes, getdate())
 END
+
+-- Validação
+SELECT * FROM Habilidade
+SELECT * FROM LogFull
+INSERT INTO Habilidade(Nome, MultiplicadorPoder) VALUES ('Nova Habilidade', 350)
+DELETE FROM Habilidade WHERE MultiplicadorPoder = '350'
