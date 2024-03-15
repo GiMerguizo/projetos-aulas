40228e096628c5e952d6504c57ca0505db742e19
Giovana Merguizo <giovana.pontes5654@hotmail.com>
2024-03-14 22:16:46 -0300

    Add new files aulas

---
+++ Semestre5/EstruturaDados/Pilha/Exercicios/ex01_validador_parenteses.py
@@ -0,0 +1,13 @@
+from Pilha import Pilha;
+
+string = str(input('String: '))
+
+tamanho = len(string)
+
+def validador_parenteses(string):
+    pilha = string.pop
+
+
+if __name__ == "__main__":
+    pilha = Pilha()
+    print(validador_parenteses(string))
---
+++ Semestre5/EstruturaDados/Pilha/Node.py
@@ -0,0 +1,4 @@
+class Node:
+    def __init__(self, dado):
+        self.value = dado
+        self.next = None
