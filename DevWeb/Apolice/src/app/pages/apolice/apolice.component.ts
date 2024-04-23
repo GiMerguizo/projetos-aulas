d8265d474d7eae1a5cd8fdc3e6019e4b447c002a
Giovana Merguizo <giovana.pontes5654@hotmail.com>
2024-04-22 21:47:57 -0300

    Apolice v1.0

---
+++ DevWeb/Apolice/src/app/pages/apolice/apolice.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-apolice',
+  templateUrl: './apolice.component.html',
+  styleUrl: './apolice.component.css'
+})
+export class ApoliceComponent {
+
+}
